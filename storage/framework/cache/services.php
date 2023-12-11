b50bca79e4977b58298b4d184d5526d8f779fa2b
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-12-11 12:59:03 +0200

    Create services.php

---
+++ storage/framework/cache/services.php
@@ -0,0 +1,63 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Illuminate\\Bus\\BusServiceProvider',
+    1 => 'Illuminate\\Cache\\CacheServiceProvider',
+    2 => 'Illuminate\\Database\\DatabaseServiceProvider',
+    3 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+    4 => 'Illuminate\\Routing\\RoutingServiceProvider',
+    5 => 'Illuminate\\Session\\SessionServiceProvider',
+    6 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
+    7 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
+    8 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
+    9 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
+    10 => 'Roots\\Acorn\\View\\ViewServiceProvider',
+    11 => 'Log1x\\AcfComposer\\Providers\\AcfComposerServiceProvider',
+    12 => 'Log1x\\Navi\\Providers\\NaviServiceProvider',
+    13 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
+    14 => 'Carbon\\Laravel\\ServiceProvider',
+    15 => 'App\\Providers\\ThemeServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Illuminate\\Database\\DatabaseServiceProvider',
+    1 => 'Illuminate\\Routing\\RoutingServiceProvider',
+    2 => 'Illuminate\\Session\\SessionServiceProvider',
+    3 => 'Roots\\Acorn\\Assets\\AssetsServiceProvider',
+    4 => 'Roots\\Acorn\\Filesystem\\FilesystemServiceProvider',
+    5 => 'Roots\\Acorn\\Providers\\AcornServiceProvider',
+    6 => 'Roots\\Acorn\\Providers\\RouteServiceProvider',
+    7 => 'Roots\\Acorn\\View\\ViewServiceProvider',
+    8 => 'Log1x\\AcfComposer\\Providers\\AcfComposerServiceProvider',
+    9 => 'Log1x\\Navi\\Providers\\NaviServiceProvider',
+    10 => 'Log1x\\SageSvg\\SageSvgServiceProvider',
+    11 => 'Carbon\\Laravel\\ServiceProvider',
+    12 => 'App\\Providers\\ThemeServiceProvider',
+  ),
+  'deferred' => 
+  array (
+    'Illuminate\\Bus\\Dispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Contracts\\Bus\\Dispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Contracts\\Bus\\QueueingDispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Bus\\BatchRepository' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Bus\\DatabaseBatchRepository' => 'Illuminate\\Bus\\BusServiceProvider',
+    'cache' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'cache.store' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'cache.psr6' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'memcached.connector' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'Illuminate\\Cache\\RateLimiter' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'Illuminate\\Contracts\\Pipeline\\Hub' => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+  ),
+  'when' => 
+  array (
+    'Illuminate\\Bus\\BusServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Cache\\CacheServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Pipeline\\PipelineServiceProvider' => 
+    array (
+    ),
+  ),
+);
